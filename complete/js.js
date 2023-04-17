82c61e68005d632459dbe7a8400e8b6e90cb66a7
Andrey Tatarnikov <andrestat95@mail.ru>
2023-04-17 20:11:29 +0300

    add change

--- complete/js.js
+++ complete/js.js
@@ -1,27 +1,30 @@
 'use strict'
 
-// function myFunction() {
-//     function myFunction2() {
-//         a = 3
-//     }
-//     myFunction2()
-// }
-// myFunction()
-
-// console.log(a)  
-
 let a
-let b
 
-function myFn() {
-    let b
-    a = true
-    b = 5
-    console.log(b)
+function myFunction() {
+    function myFunction2() {
+        a = 3
+    }
+    myFunction2()
 }
+myFunction()
 
-myFn()
+console.log(a)  
 
-console.log(a)
-console.log(b)
+
+// let a
+// let b
+
+// function myFn() {
+//     let b
+//     a = true
+//     b = 5
+//     console.log(b)
+// }
+
+// myFn()
+
+// console.log(a)
+// console.log(b)
 
